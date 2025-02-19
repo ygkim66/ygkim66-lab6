0527eabaa5901448c034ee84e6504d7568afbb82
Yeana (Y.G.) Kim <ygkcontact@gmail.com>
2025-02-18 22:46:27 -0500

    first attempt encrypt method

--- Enigma.java
+++ Enigma.java
@@ -1,6 +1,7 @@
 public class Enigma{
 
-    private String rotorInit[] = {"#GNUAHOVBIPWCJQXDKRYELSZFMT",
+    private String rotorInit[] = 
+    {"#GNUAHOVBIPWCJQXDKRYELSZFMT",
         "#EJOTYCHMRWAFKPUZDINSXBGLQV",
         "#BDFHJLNPRTVXZACEGIKMOQSUWY",
         "#NWDKHGXZVRIFJBLMAOPSCYUTQE",
@@ -21,18 +22,40 @@ public class Enigma{
 
     public String decrypt(String message){        
         //TODO
+        String dec = "";
+        return dec;
     }
 
 
     
     public String encrypt(String message){
         //TODO
+        String enc = "";
+        int innerCount = -1;
+        int outerCount = -1;
+        int middleCount = -1;
+        char innerRotor = 0;
+        char outerRotor = 0;
+        char middleRotor = 0;
+        String step1 = "";
+        for (int i = 0; i < message.length(); i++){
+            //finds location of first letter in message
+            innerCount = rotors[0].indexOf(message.charAt(i));
+            outerRotor = rotors[2].charAt(innerCount);
+            middleCount = rotors[1].indexOf(outerRotor);
+            enc = enc + rotors[2].charAt(middleCount);
+
+            rotate();
+        }
+        return enc;
     }
 
     
     private void rotate(){
         if(rotors[0].rotate()){
+
             if(rotors[1].rotate()){
+
                 rotors[2].rotate();
             }
         }
